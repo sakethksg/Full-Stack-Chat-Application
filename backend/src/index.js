0a647e9ccec03b61406b4471e0f9562c9cbf94cc
sakethksg <ksgsaketh@gmail.com>
2025-04-20 11:34:10 +0530

    update cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,10 +19,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+  cors({})
 );
 
 app.use("/api/auth", authRoutes);
