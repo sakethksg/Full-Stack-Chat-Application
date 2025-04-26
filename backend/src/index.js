c53bcf37d9162ebbe2830417f2772bfca61a4830
Saketh <ksgsaketh@gmail.com>
2025-04-26 11:40:06 +0530

    Fix bugs: increase NGINX client_max_body_size, update CORS configuration, add 50x error page, remove obsolete docker-compose version

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: "10mb" })); // Increase the limit to 10MB or as ne
 app.use(express.urlencoded({ limit: "10mb", extended: true })); // For URL-encoded data
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "http://localhost", "http://localhost:80"],
   credentials: true, 
 }));
 
